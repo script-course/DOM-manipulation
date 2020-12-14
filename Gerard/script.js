3a933a35bf591bbd0a7dca3c85b06a7ec8ddadda
Gerard Oosterhof <gerard@script.nl>
2020-12-14 14:46:38 +0100

    Removed prettier and using code-workspace settings

--- Gerard/script.js
+++ Gerard/script.js
@@ -1,16 +1,16 @@
-const NAME = "Gerard";
+const NAME = 'Gerard';
 
-const nameElemenent = document.getElementById("name");
+const nameElemenent = document.getElementById('name');
 
-const randomUint8 = () => Math.floor(Math.random() * 255)
+const randomUint8 = () => Math.floor(Math.random() * 255);
 const randomColor = () => `rgb(${randomUint8()}, ${randomUint8()}, ${randomUint8()})`;
 
-document.getElementById("click").onclick = () => {
-    if(nameElemenent.innerHTML) {
-        nameElemenent.innerHTML = ''
-        return
+document.getElementById('click').onclick = () => {
+    if (nameElemenent.innerHTML) {
+        nameElemenent.innerHTML = '';
+        return;
     }
 
     nameElemenent.innerHTML = NAME;
-    nameElemenent.style.color = randomColor()
+    nameElemenent.style.color = randomColor();
 };
